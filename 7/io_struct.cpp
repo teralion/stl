acc1e19d7e5ad2aa170d95ba977fcf64b3ee7106
teralion <slavad878@gmail.com>
2022-10-26 11:46:44 +0300

    io manip

---
+++ 7/io_struct.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+struct city {
+  string name;
+  size_t population;
+  double latitude;
+  double longtitude;
+};
+
+istream& operator>>(istream &is, city &c)
+{
+  is >> ws;
+  getline(is, c.name);
+  is >> c.population
+    >> c.latitude
+    >> c.longtitude;
+
+  return is;
+}
+
+int main()
+{
+  vector<city> l;
+  copy(istream_iterator<city>{cin}, {},
+    back_inserter(l));
+
+  for (const auto &[name, pop, lat, lon] : l) {
+    cout << left << setw(15) << name
+      << " population=" << pop
+      << " lat=" << lat
+      << " lon=" << lon << '\n';
+  }
+}
